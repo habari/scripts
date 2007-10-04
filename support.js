efea0843dfd1c6db1e59f4d3ce5e9b97a2b0601a
Owen Winkler <epithet@gmail.com>
2007-10-04 12:27:55 +0000

    Allow draggable resizing of publishing content area.  Needs a grip handle graphic of some kind.
    git-svn-id: https://svn.habariproject.org/habari/trunk/htdocs/scripts@957 653ae4dd-d31e-0410-96ef-6bf7bf53c507

--- support.js
+++ support.js
@@ -2,3 +2,28 @@ function popUp(url) {
  	window.open (url, "mywindow",
 				 "location=0,status=1,scrollbars=1, width=700,height=500");
 }
+
+$(document).ready(function() {
+	$('textarea.resizable').each(function() {
+		var textarea = $(this);
+		var offset = null;
+		var grip = $('<div class="grip"></div>').mousedown(function(ev){
+			offset = textarea.height() - (ev.clientY + document.documentElement.scrollTop)
+			textarea.css('opacity', 0.25);
+			$(document).mousemove(doDrag).mouseup(endDrag);
+		}).mouseup(endDrag);
+		var resizer = $('<div class="resizer"></div>').css('margin-bottom',$(this).css('margin-bottom'));
+		$(this).css('margin-bottom', '0px').wrap(resizer).parent().append(grip);
+		
+		function doDrag(ev){
+			textarea.height(Math.max(offset + ev.clientY + document.documentElement.scrollTop, 60) + 'px');
+			return false;
+		}
+		
+		function endDrag(ev){
+			$(document).unbind('mousemove', doDrag).unbind('mouseup', endDrag);
+			textarea.css('opacity', 1.0);
+		}
+		
+	});
+});
