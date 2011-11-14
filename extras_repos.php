44e245173b3391378fae855eaeef568101f28a7b
mikelietz <codger+git@gmail.com>
2011-11-13 23:09:14 -0500

    Since the v3 API doesn't seem to paginate like it says it does, things can be a lot simpler.

--- extras_repos.php
+++ extras_repos.php
@@ -2,25 +2,13 @@
 
 $api_endpoint = 'https://api.github.com/';
 
-$url = $api_endpoint . "orgs/habari-extras";
-
+$url = $api_endpoint . "orgs/habari-extras/repos";
 $result = file_get_contents( $url );
+$repos = json_decode ( $result );
 
-$result = json_decode ( $result );
+$extras_repositories = array();
 
-if ( isset( $result->public_repos ) and $result->public_repos > 0 ) {
-	$url = $api_endpoint . "orgs/habari-extras/repos";
-
-//	$url .= "?per_page=100"; // this does not seem to actually be paginating currently, so this is unnecessary
-
-	$result = file_get_contents( $url );
-
-	$repos = json_decode ( $result );
-
-	$count = 1;
-	foreach( $repos as $repo ) {
-		echo $count++ . ". {$repo->name}\n";
-
-	}
+foreach( $repos as $repo ) {
+	$extras_repositories[] = $repo->name;
 }
 ?>
