2f19048391a653fef9dee44f7f14734ff7083ce4
Matt Read <matt@mattread.com>
2012-08-31 19:43:40 +0000

    update xgettext script

--- rxgettext.sh
+++ rxgettext.sh
@@ -1,1 +1,3 @@
-xgettext -o system/locale/habari.pot --no-wrap --add-comments=@locale --language=PHP --from-code=utf-8 --keyword=_n:1,2 --keyword=_ne:1,2 --keyword=_t --keyword=_e $(find . -name "*.php")
+xgettext -o locale/habari.pot --no-wrap --add-comments=@locale --language=PHP --from-code=utf-8 --keyword=_n:1,2 --keyword=_ne:1,2 --keyword=_t --keyword=_e $(find . -name "*.php")
+xgettext -o locale/habari.pot --no-wrap --add-comments=@locale --language=Perl --join-existing --from-code=utf-8 --keyword=_n:1,2 --keyword=_ne:1,2 --keyword=_t --keyword=_e $(find . -name "*.js")
+
